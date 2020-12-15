cc1aec1439482ee5788b0b6fa0fb0f324dd52677
Angeline Jacob <angeline.jacob.17@gmail.com>
2020-12-15 17:05:20 -0500

    adjusted some routes to return populated blogs

--- controllers/blogsController.js
+++ controllers/blogsController.js
@@ -5,7 +5,9 @@ const User = require('../models/user')
 // CREATE ROUTE
 router.post('/:id', (req, res) => {
     console.log("Hit post route to create a blog")
-    User.findById(req.params.id, (error, foundUser) => {
+    User.findById(req.params.id)
+    .populate('blogs')
+    .exec((error, foundUser) => {
         if(error){
             console.log("error while creating new blog >> ", error)
         }
@@ -26,11 +28,38 @@ router.post('/:id', (req, res) => {
                     createdBlog.save(function(error, savedUser){
                         if(error) console.log(error)
                     })
-                    res.json(createdBlog)
+                    res.json(foundUser)
                 }
             })
         }
     })
+
+    // User.findById(req.params.id, (error, foundUser) => {
+    //     if(error){
+    //         console.log("error while creating new blog >> ", error)
+    //     }
+    //     else {
+    //         console.log(foundUser)
+    //         // create the new blog and add it to user's blog array
+    //         Blog.create(req.body, (error, createdBlog) => {
+    //             if(error){
+    //                 console.log(error)
+    //             }
+    //             else{
+    //                 foundUser.blogs.push(createdBlog)
+    //                 foundUser.save(function(error, savedUser){
+    //                     if(error) console.log(error)
+    //                 })
+    //                 // add foundUser as a reference to the blog we just created
+    //                 createdBlog.author = foundUser
+    //                 createdBlog.save(function(error, savedUser){
+    //                     if(error) console.log(error)
+    //                 })
+    //                 res.json(foundUser)
+    //             }
+    //         })
+    //     }
+    // })
     
 })
 
